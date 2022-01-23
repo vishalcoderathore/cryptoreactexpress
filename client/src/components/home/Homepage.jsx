8743285923ef313481b044c709404c1f4564e524
Vishal Rathore <vshl.rathore87@gmail.com>
2022-01-22 21:07:25 -0600

    Convert Homepage to class component

--- client/src/components/home/Homepage.jsx
+++ client/src/components/home/Homepage.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 
 const { Title } = Typography;
 
-export default function Homepage() {
-  return (
-    <>
-      <Title level={2} className='heading'>
-        Global Crypto Stats
-      </Title>
-      <Row gutter={[24, 24]}>
-        <Col span={12}>
-          <Statistic title='Total Currencies' value='5' />
-        </Col>
-        <Col span={12}>
-          <Statistic title='Total Exchanges' value='5' />
-        </Col>
-        <Col span={12}>
-          <Statistic title='Total Market Cap' value='5' />
-        </Col>
-        <Col span={12}>
-          <Statistic title='Total 24h Volume' value='5' />
-        </Col>
-        <Col span={12}>
-          <Statistic title='Total Markets' value='5' />
-        </Col>
-      </Row>
-    </>
-  );
+class Homepage extends Component {
+  render() {
+    return (
+      <>
+        <Title level={2} className='heading'>
+          Global Crypto Stats
+        </Title>
+        <Row gutter={[24, 24]}>
+          <Col span={12}>
+            <Statistic title='Total Currencies' value='5' />
+          </Col>
+          <Col span={12}>
+            <Statistic title='Total Exchanges' value='5' />
+          </Col>
+          <Col span={12}>
+            <Statistic title='Total Market Cap' value='5' />
+          </Col>
+          <Col span={12}>
+            <Statistic title='Total 24h Volume' value='5' />
+          </Col>
+          <Col span={12}>
+            <Statistic title='Total Markets' value='5' />
+          </Col>
+        </Row>
+      </>
+    );
+  }
 }
+
+export default Homepage;
